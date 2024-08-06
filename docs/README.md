b7b7ab94ebf49ac28c66d6dd258d07bce9dc0490
Andy Sit <andysit173@gmail.com>
2024-08-06 16:51:40 -0400

    init

---
+++ docs/README.md
@@ -0,0 +1,1 @@
+not created yet, when will project is more defined and figured out
---
+++ src/config.py
@@ -0,0 +1,13 @@
+
+
+"""
+  Hold basic directory names and such to make this server easier to handle filenames
+"""
+
+
+BATSCRIPTS = "../bat_scripts/"
+
+
+
+
+
