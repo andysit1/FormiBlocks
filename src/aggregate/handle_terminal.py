75ae547c9f53e32d2fcc3582c0c55e90afe54bc5
Andy Sit <andysit173@gmail.com>
2024-08-08 21:29:44 -0400

    added searching program names

--- src/aggregate/handle_terminal.py
+++ src/aggregate/handle_terminal.py
@@ -1,6 +1,63 @@
 import shutil
 import subprocess
 import os
+import re
+from icecream import ic
+program_path = "C:/Users/andys/AppData/Roaming/Microsoft/Windows/Start Menu/Programs"
+
+class FileSystem:
+    def __init__(self) -> None:
+        self.program_dict = {}
+        self.cache_programs()
+        self.find_programs()
+
+        self.not_program = re.compile(pattern="uninstall")
+
+    def find_programs(self):
+        """
+        Recursively finds all .lnk shortcut files in the given directory.
+
+        :param root_dir: The directory to start searching from
+        :return: List of paths to .lnk files
+        """
+
+
+        for root, dirs, files in os.walk(program_path):
+            for file in files:
+                if file.lower().endswith('.lnk'):
+                    self.program_dict[file.lower()] = os.path.join(root, file)
+
+                if file.lower().endswith('.url'):
+                    self.program_dict[file.lower()] = os.path.join(root, file)
+
+    def search_key(self, key : str):
+
+        regex_cmd = re.compile(pattern="({})".format(key.lower()))
+        list_of_match = []
+        for key in self.program_dict.keys():
+            found : list = regex_cmd.findall(key)
+
+            if found and not self.not_program.findall(key):
+              list_of_match.append(key)
+
+            # in the event that we do have similar program names we should prob return the list and get the user to pick the correct program
+
+        ic(list_of_match, len(list_of_match))
+
+        if len(list_of_match) == 1:
+            return self.program_dict[list_of_match[0]]
+
+        if len(list_of_match) == 0:
+            return None
+
+        raise TypeError("need a more specific key word to find key")
+
+    #on start up
+    def cache_programs(self):
+        pass
+
+    def get_programs(self):
+        return self.program_dict
 
 def find_shortcuts(root_dir):
     """
@@ -21,6 +78,9 @@ def find_shortcuts(root_dir):
     return shortcuts
 
 
+
+    start
+
 """
   steam://rungameid/{id of game}
   we could just find a way to regex and pin the id to the game then call steam://rungameid/id of game
@@ -28,23 +88,8 @@ def find_shortcuts(root_dir):
 """
 
 if __name__ == "__main__":
-  print("testing terminal commands / subprocess")
-  desktop_path = os.path.join(os.path.expanduser("~"), "Desktop")
-  print(desktop_path)
-  executable_path = shutil.which("echo")
-  print(executable_path)
-
-  # Set the root directory to search from
-  root_directory = "C:/"  # Change this to the directory you want to search
-  game_dir = "C:/Users/andys/AppData/Roaming/Microsoft/Windows/Start Menu/Programs/Steam"
-  program_dir = "C:/Users/andys/AppData/Roaming/Microsoft/Windows/Start Menu/Programs"
-
-  shortcuts = find_shortcuts(program_dir)
-
-  # Print out the list of shortcuts
-  print(f"Found {len(shortcuts)} shortcut files:")
-  for shortcut in shortcuts:
-      print(shortcut)
+  fs = FileSystem()
+  print(fs.search_key("apex")) # returns C:/Users/andys/AppData/Roaming/Microsoft/Windows/Start Menu/Programs\Steam\Apex Legends.url
 
 
   """
